e8b87894f767f7caffb79d40ac522ad8dd4af2b7
macvandu <du132013@gmail.com>
2026-03-11 15:05:06 +0700

    add Ball

---
+++ ball/flat-baseball/scripts/c3main.js
@@ -0,0 +1,4 @@
+import "./c3runtime.js";
+import "./plugins/skymen_pokiSDK/c3runtime/main.js";
+import "./objRefTable.js";
+import "./project/javaScriptInEvents.js";
---
+++ ball/flat-baseball/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,12 @@
+
+
+
+const scriptsInEvents = {
+
+	async Loadingevt_Event3_Act1(runtime, localVars)
+	{
+		console.log = function () {};
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
